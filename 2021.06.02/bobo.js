58c48a50a7a175a6ddc1d6554de24d2242131d7b
Ihor <ihorg@nayax.com>
2021-06-08 08:27:40 +0300

    home work lesson 2

---
+++ 2021.06.02/bobo.js
@@ -0,0 +1,7 @@
+function validateForm() {
+	var x = document.forms["infoForm"]["finfotext"].value;
+	if(x == "print") {
+       alert("\\Huk\\ \n Ihor \n \* 02 \n :)  Odessa");
+	}
+    return false;
+}
